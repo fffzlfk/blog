660eddff6fc9024c5000f9496323db0824a6ea8e
fffzlfk <1319933925qq@gmail.com>
2022-01-05 23:26:00 +0800

    Fix Dark Mode height

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Meta from './meta'
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-white dark:bg-[#272727]">
+    <div className="bg-white dark:bg-[#272727] pb-1">
       <Meta />
       <div className="min-h-screen">
         <Alert />
