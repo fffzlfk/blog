0e9a243a004a8258f2576ab5a423bdabca16257f
fffzlfk <1319933925qq@gmail.com>
2022-01-05 23:02:55 +0800

    Fix Dark Mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/global.css'
+import { ThemeProvider } from 'next-themes'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
