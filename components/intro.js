70d593ec5a01d4a63c7888387312f63b2eff4eca
fffzlfk <1319933925qq@gmail.com>
2022-01-08 22:59:48 +0800

    Fix UI

--- components/intro.js
+++ components/intro.js
@@ -1,10 +1,9 @@
 import { HOME_NAME, SLOGAN } from "../lib/constant";
-import H3 from "./h3";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-left md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-gray-900 dark:text-white text-7xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="flex-col md:flex-row flex items-left md:justify-between py-16">
+      <h1 className="text-gray-900 dark:text-white text-8xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         {HOME_NAME}
       </h1>
       <h3 className="text-gray-600 dark:text-gray-300 text-3xl md:text-left text-lg mt-5 md:pl-8">
--- components/post-header.js
+++ components/post-header.js
@@ -3,7 +3,7 @@ import PostTitle from '../components/post-title'
 
 export default function PostHeader({ title, date }) {
   return (
-    <div className="py-10 flex flex-col items-center justify-center">
+    <div className="pt-10 flex flex-col items-center justify-center">
       <PostTitle>{title}</PostTitle>
       <div className="max-w-2xl mx-auto">
         <div className="mb-6 text-lg">
